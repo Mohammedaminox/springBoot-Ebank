a8271021e7e2cde6a48246a8a01dcb2e8bed2c17
amineBd <aminebenmade1999@gmail.com>
2025-01-14 17:33:31 +0100

    configure the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import {UnauthorizedComponent} from "./unauthorized/unauthorized.component";
+import {RoleGuard} from "./guard/roleGuard/role.guard";
+import {HomeComponent} from "./home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
+    canActivate: [RoleGuard],
+    data: {  role: 'ADMIN' }
+  },
+
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.module').then(m => m.UserModule),
+    canActivate: [RoleGuard],
+    data: { role: 'USER' }
+  },
+
+  { path: 'unauthorized', component: UnauthorizedComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' },
+ ];
